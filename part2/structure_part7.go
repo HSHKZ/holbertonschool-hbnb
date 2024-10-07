869e9e488fca64339d8c2ee18cb425b189227c50
HSHKZ <lucasolegniel@gmail.com>
2024-10-07 19:22:11 +0200

    create the structure part 7 base for the HBnB part 2

---
+++ part2/structure_part7.go
@@ -0,0 +1,21 @@
+hbnb/
+├── app/
+│   ├── api/
+│   │   ├── __init__.py
+│   │   ├── 1.place_api.py
+│   │   ├── 1.review_api.py
+│   ├── errors.py
+├── models/
+│   ├── __init__.py
+│   ├── 1.hbnb_model.py
+│   ├── 1.place_model.py
+│   ├── 1.amenity_model.py
+│   ├── 1.user_model.py
+│   └── 1.review_model.py
+├── services/
+│   ├── __init__.py
+│   ├── 1.place_service.py
+│   └── 1.review_service.py
+└── tests/
+    ├── __init__.py
+    └── 1.test_review_api.py
