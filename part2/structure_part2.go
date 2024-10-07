56c55b56d81007eb205557218a2ecfc34dc4fe62
HSHKZ <lucasolegniel@gmail.com>
2024-10-07 13:09:46 +0200

    create the structure part 2 base for the HBnB part 2

---
+++ part2/structure_part2.go
@@ -0,0 +1,16 @@
+hbnb/
+├── app/
+│   ├── models/
+│   │   ├── 11.init.py
+│   │   ├── 12.hbnb_model.py
+│   │   ├── 13.user.py
+│   │   ├── 14.place.py
+│   │   ├── 15.review.py
+│   │   └── 16.amenity.py
+├── config/
+│   ├── 15.init.py
+│   └── 16.settings.py
+├── 17.run.py
+└── tests/
+    ├── 18.init.py
+    └── 19.test_hbnb.py
