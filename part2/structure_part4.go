5d00763625cb0c0687acccdc932f9754b75fffcf
HSHKZ <lucasolegniel@gmail.com>
2024-10-07 14:17:32 +0200

    create the structure part 4 base for the HBnB part 2

---
+++ part2/structure_part4.go
@@ -0,0 +1,15 @@
+hbnb/
+├── app/
+│   ├── api/
+│   │   ├── 21.init.py
+│   │   ├── 28.amenity.py
+│   └── 23.errors.py
+├── models/
+│   ├── 12.hbnb_model.py
+│   ├── 29.amenity.py
+├── services/
+│   ├── 24.init.py
+│   └── 30.amenity_service.py
+└── tests/
+    ├── 26.init.py
+    └── 31.test_amenity.py
